447314999576bc2990a234b4b85b292be30fff9f
DauletKalesh <dauletkalesov@gmail.com>
2024-08-22 01:27:27 +0500

    two application done

--- news/serializers.py
+++ news/serializers.py
@@ -13,9 +13,17 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class NewsSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
+    tags = TagSerializer(many=True)
+    category = CategorySerializer()
+
+    class Meta:
+        model = News
+        fields = '__all__'
+
+class UserNewsSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
     category = CategorySerializer(read_only=True)
 
     class Meta:
         model = News
-        fields = '__all__'
+        exclude = ('is_published', 'author', 'last_modified_date',)
--- news/views.py
+++ news/views.py
@@ -1,16 +1,30 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from .models import News, Category, Tag
-from .serializers import NewsSerializer, CategorySerializer, TagSerializer
+from .serializers import NewsSerializer, CategorySerializer, TagSerializer, UserNewsSerializer
+from rest_framework import filters
 
 class NewsViewSet(viewsets.ModelViewSet):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content', 'tags__name', 'category__name']
 
     def get_permissions(self):
         if self.request.user.is_staff:
             return [IsAdminUser()]
         return [IsAuthenticatedOrReadOnly()]
+    
+    def get_serializer_class(self):
+        if not self.request.user.is_staff:
+            return UserNewsSerializer
+        return self.serializer_class
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(is_published=True)
+        return queryset
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
